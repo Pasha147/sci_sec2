e9de73ec9299cf8bf695dd457d16197ae25e7022
Pasha <75729255+Pasha147@users.noreply.github.com>
2024-04-12 11:37:09 +0300

    Update news.js

--- news.js
+++ news.js
@@ -1,12 +1,17 @@
 export const news = [
     {
-        date: '2024',
-        title: '1 sdfsdfsf',
-        text: 'sdgfdgdgfdgg'
+        date: '2024\04\12',
+        title: 'Новина 1',
+        text: 'Всі накуові співробітники сьогодні працюють добре!'
     },
     {
         date: '2024',
         title: '2 sdfsdfsf',
+        text: 'sdgfdgdgfdgg'
+    },
+    {
+        date: '2024',
+        title: '3 sdfsdfsf',
         text: 'sdgfdgdgfdgg'
     },
     {
